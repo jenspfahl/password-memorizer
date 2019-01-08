331e25690505521885bafdfb0a71c21ab7c0f927
Jens Pfahl <jens.pfahl@mailbox.org>
2019-01-08 19:09:33 +0100

    Rename project name

--- src/main/java/de/jepfa/obfusser/Constants.java
+++ src/main/java/de/jepfa/obfusser/Constants.java
@@ -3,7 +3,7 @@ package de.jepfa.obfusser;
 public interface Constants {
 
     String NL = System.getProperty("line.separator");
-    String VERSION = "0.1.1";
+    String VERSION = "0.9.1";
 
     int NO_ID = Integer.MIN_VALUE;
     String NO_GROUP_NAME = "-no group-";
